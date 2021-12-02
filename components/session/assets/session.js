d8eff8070a64083512830fa49a12ba86b1b92b16
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-12-02 16:51:47 +0100

    Added checks for permissions

--- components/session/assets/session.js
+++ components/session/assets/session.js
@@ -8,28 +8,39 @@ $(document).ready(() => {
     var component = document.getElementById('ov-stream-publisher');
     var token = component.getAttribute('data-token');
 
-    //
-    OV = new OpenVidu();
-    session = OV.initSession();
-
-    //
-    session.on('streamCreated', function (event) {
-        console.log('Nieuwe deelnemer');
-        console.log(event);
-        var subscriber = session.subscribe(event.stream, 'subscriber');
-
-        subscriber.createVideoElement(document.getElementById('ov-stream-subscriber'), 'APPEND');
-    });
-
-    //
-    session.connect(token)
-        .then(() => {
-            var publisher = OV.initPublisher("publisher");
-            session.publish(publisher);
-
-            publisher.addVideoElement(document.getElementById('ov-stream-publisher'), 'APPEND');
+    // Check is permissions are granted
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true})
+        .then(function(stream) {
+            setupOVSession();
         })
-        .catch(error => {
-            console.log("There was an error connecting to the session:", error.code, error.message);
+        .catch(function(err) {
+            document.getElementById('ov-error-message').innerHTML = err.message;
         });
+
+    function setupOVSession() {
+        //
+        OV = new OpenVidu();
+        session = OV.initSession();
+
+        //
+        session.on('streamCreated', function (event) {
+            var subscriber = session.subscribe(event.stream, 'subscriber');
+
+            subscriber.createVideoElement(document.getElementById('ov-stream-subscriber'), 'APPEND');
+            document.getElementById('ov-stream-subscriber-wrapper').classList.remove('hidden');
+        });
+
+        //
+        session.connect(token)
+            .then(() => {
+                var publisher = OV.initPublisher("publisher");
+                session.publish(publisher);
+
+                publisher.addVideoElement(document.getElementById('ov-stream-publisher'), 'APPEND');
+                document.getElementById('ov-stream-publisher-wrapper').classList.remove('hidden');
+            })
+            .catch(error => {
+                console.log("There was an error connecting to the session:", error.code, error.message);
+            });
+    }
 });
